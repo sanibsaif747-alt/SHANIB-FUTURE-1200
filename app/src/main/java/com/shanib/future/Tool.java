711a3232e58f088aa412bf2cedc43ec96d784a1b
SHAANIB <sanibsaif747@gmail.com>
2026-08-22 19:26:59 +0000

    feat: SHANIB FUTURE 1200 Native - 1200 tools by SHAANIB - real native APK

---
+++ app/src/main/java/com/shanib/future/Tool.java
@@ -0,0 +1,7 @@
+package com.shanib.future;
+public class Tool {
+    public String id, name, cat, desc, howToUse, example;
+    public Tool(String id, String name, String cat, String desc, String howToUse, String example) {
+        this.id=id; this.name=name; this.cat=cat; this.desc=desc; this.howToUse=howToUse; this.example=example;
+    }
+}
